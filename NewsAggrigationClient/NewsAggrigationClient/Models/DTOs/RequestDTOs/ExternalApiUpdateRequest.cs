bbada3a59d849c20ba1d0426c9dc4edd9ff3385b
Mahaveer Parakh <mahaveer.parakh@intimetec.com>
2025-07-17 00:35:34 +0530

    Updated request and response DTO for including some extra properties

--- NewsAggrigationClient/NewsAggrigationClient/Models/DTOs/RequestDTOs/ExternalApiUpdateRequest.cs
+++ NewsAggrigationClient/NewsAggrigationClient/Models/DTOs/RequestDTOs/ExternalApiUpdateRequest.cs
@@ -8,8 +8,9 @@ namespace NewsAggrigationClient.Models.DTOs.RequestDTOs
 {
     public class ExternalApiUpdateRequest
     {
+        public int ApiId { get; set; }
         public string? ApiName { get; set; }
-        public string? BaseUrl { get; set; }
+        public string? ApiUrl { get; set; }
         public string? ApiKey { get; set; }
     }
 }
---
+++ NewsAggrigationService/NewsAggrigation.API/DataDTOs/CategoryStatusDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsAggrigation.API.DataDTOs
+{
+    public class CategoryStatusDto
+    {
+        public string CategoryName { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
